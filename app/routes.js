fc61487753550d8ff406c2d265a89c66864047de
Agz <agz@dxw.com>
2018-11-24 12:22:15 +0000

    Add basic sub-nav and routes

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/past-entries', function (req, res) {
+  res.render('past-entries')
+})
+
+router.get('/new-diary-entry', function (req, res) {
+  res.render('new-diary-entry')
+})
+
+router.get('/help-guide', function (req, res) {
+  res.render('help-guide')
+})
 
 module.exports = router
